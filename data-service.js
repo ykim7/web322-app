65eff4ca4b09b86a59cc06e88df89799d3670617
Yujin Kim <gettheradio@gmail.com>
2022-10-14 23:45:18 -0400

    assignment03

--- data-service.js
+++ data-service.js
@@ -5,23 +5,18 @@ var programs = [];
 
 module.exports.initialize = () => {
     return new Promise((resolve, reject) => {
-        try {
-            fs.readFile("./data/students.json", "utf8", (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                students = JSON.parse(data);
-            });
-            fs.readFile("./data/programs.json", "utf8", (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                programs = JSON.parse(data);
-            });
-        } catch (err) {
-            reject("Unable to read file");
-        }
-
+        fs.readFile("./data/students.json", "utf8", (err, data) => {
+            if (err) {
+                reject("Unable to read file");
+            }
+            students = JSON.parse(data);
+        });
+        fs.readFile("./data/programs.json", "utf8", (err, data) => {
+            if (err) {
+                reject("Unable to read file");
+            }
+            programs = JSON.parse(data);
+        });
         resolve();
     });
 };
@@ -56,5 +51,67 @@ module.exports.getPrograms = () => {
             return;
         }
         resolve(programs);
+    });
+};
+
+module.exports.addStudent = (studentData) => {
+    return new Promise((resolve, reject) => {
+        if (studentData.isInternationalStudent == undefined) {
+            studentData.isInternationalStudent = false;
+        } else {
+            studentData.isInternationalStudent = true;
+        }
+
+        const allStudentIDs = students.map((student) => {
+            return parseInt(student.studentID);
+        });
+        studentData.studentID = (Math.max(...allStudentIDs) + 1).toString();
+        students.push(studentData);
+        resolve();
+    });
+};
+
+module.exports.getStudentsByStatus = (status) => {
+    return new Promise((resolve, reject) => {
+        const studentsByStatus = students.filter((student) => {
+            return student.status == status;
+        });
+        if (studentsByStatus.length == 0) {
+            reject("no results returned");
+        }
+        resolve(studentsByStatus);
+    });
+};
+module.exports.getStudentsByProgramCode = (programCode) => {
+    return new Promise((resolve, reject) => {
+        const studentsByProgram = students.filter((student) => {
+            return student.program == programCode;
+        });
+        if (studentsByProgram.length == 0) {
+            reject("no results returned");
+        }
+        resolve(studentsByProgram);
+    });
+};
+module.exports.getStudentsByExpectedCredential = (credential) => {
+    return new Promise((resolve, reject) => {
+        const studentsByCredential = students.filter((student) => {
+            return student.expectedCredential == credential;
+        });
+        if (studentsByCredential.length == 0) {
+            reject("no results returned");
+        }
+        resolve(studentsByCredential);
+    });
+};
+module.exports.getStudentById = (sid) => {
+    return new Promise((resolve, reject) => {
+        const studentsById = students.filter((student) => {
+            return student.studentID == sid;
+        });
+        if (studentsById.length == 0) {
+            reject("no results returned");
+        }
+        resolve(studentsById);
     });
 };
--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 /*********************************************************************************
- *  WEB322 – Assignment 02
+ *  WEB322 – Assignment 03
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part
  *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
  *  Name: Yujin Kim / Student ID: ykim296 / Date: 2022 Sep 29
  *
- *  Online (Cyclic) Link: https://elated-mite-trunks.cyclic.app
+ *  Online (Cyclic) Link:
  *
  ********************************************************************************/
 
 var HTTP_PORT = process.env.PORT || 8080;
 
 var express = require("express");
+var multer = require("multer");
 var app = express();
 const path = require("path");
-
+const fs = require("fs");
 const dataService = require("./data-service.js");
-
-app.use(express.static("public"));
 
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({ storage: storage });
+
+// Routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 });
@@ -33,14 +44,89 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/students", (req, res) => {
+    if (req.query.status) {
+        const status = req.query.status;
+        dataService
+            .getStudentsByStatus(status)
+            .then((data) => {
+                res.json(data);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    } else if (req.query.program) {
+        const program = req.query.program;
+        dataService
+            .getStudentsByProgramCode(program)
+            .then((data) => {
+                res.json(data);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    } else if (req.query.credential) {
+        const credential = req.query.credential;
+        dataService
+            .getStudentsByExpectedCredential(credential)
+            .then((data) => {
+                res.json(data);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    } else {
+        dataService
+            .getAllstudents()
+            .then((data) => {
+                res.json(data);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    }
+});
+app.get("/student/:value", function (req, res) {
+    var sid = req.params.value;
     dataService
-        .getAllstudents()
+        .getStudentById(sid)
         .then((data) => {
             res.json(data);
         })
         .catch((err) => {
             res.json(err);
         });
+});
+
+app.post("/students/add", (req, res) => {
+    console.log("here!");
+    dataService
+        .addStudent(req.body)
+        .then(() => {
+            res.redirect("/students");
+        })
+        .catch((err) => {
+            console.log("Error: ", err);
+        });
+});
+
+app.get("/students/add", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/addStudent.html"));
+});
+
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+});
+
+app.get("/images/add", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/addImage.html"));
+});
+
+app.get("/images", (req, res) => {
+    const images = [];
+    fs.readdir("./public/images/uploaded", function (err, items) {
+        images.push(items);
+        res.json({ images });
+    });
 });
 
 app.get("/intlstudents", (req, res) => {
